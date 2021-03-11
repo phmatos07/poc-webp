2fbbdfdb3ece49242c8280f3a4600876b4627d2a
7700250090 <phelipe.matos-ext@viavarejo.com.br>
2021-03-11 18:06:46 -0300

    Definição de alguns ENUM para ajudar no desenvolvimento

---
+++ src/enum/architecture.enum.js
@@ -0,0 +1,6 @@
+const ArchitectureEnum = {
+  X32: 'x32',
+  X64: 'x64'
+};
+
+module.exports = ArchitectureEnum;
---
+++ src/enum/operating-system.enum.js
@@ -0,0 +1,7 @@
+const OperatingSystemEnum = {
+  MAC_OSX: 'darwin',
+  LINUX: 'linux',
+  WINDOWS: 'win32'
+};
+
+module.exports = OperatingSystemEnum;
