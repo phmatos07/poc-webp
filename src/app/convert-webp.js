acfcd13a36d080d6ebfce852847c58d22f723d87
7700250090 <phelipe.matos-ext@viavarejo.com.br>
2021-03-12 15:46:17 -0300

    Inserindo descrição sobre a função e movendo para a pasta APP

--- src/app/convert-webp.js
+++ src/app/convert-webp.js
@@ -2,6 +2,10 @@ const exec = require('child_process').execFile;
 const GetBinService = require('./../shared/services/get-bin.service');
 const BinNameEnum = require('./../shared/enum/bin-name.enum');
 
+/**
+ * @description ConvertWebp (cwebp) - Comprimir um arquivo de imagem em um arquivo WebP
+ * @returns Promise
+ */
 const ConvertWebp = (inputImage, outputImage, option = '-q 70 -quiet') => {
 
   const STRENGTH_NAME = '-o'; // Força a especificação do nome referente ao arquivo WebP de saída
@@ -15,7 +19,10 @@ const ConvertWebp = (inputImage, outputImage, option = '-q 70 -quiet') => {
         }
         resolve(stdout ? stdout : stderr);
       } catch (error) {
+        console.group(`Comando Executado: ${query}`);
+        console.log('----------------------------------------------------------------');
         console.warn(error);
+        console.groupEnd();
       }
     });
   });
